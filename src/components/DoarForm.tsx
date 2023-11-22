6d97984e79eb8c2fc67f9faf20b482bf3327da30
Gabriel Pelincel <gabrielpelincel@gmail.com>
2023-11-22 16:24:00 -0300

    Build fix

--- src/components/DoarForm.tsx
+++ src/components/DoarForm.tsx
@@ -18,31 +18,90 @@ export default function DoarForm() {
   const [rua, setRua] = useState("");
   const [numero, setNumero] = useState("");
   const [imageUpload, setImageUpload] = useState<File | undefined>();
-  const [imageurl, setImageurl] = useState("");
+  const [imageurl, setImageurl] = useState<string | null>(null);
 
-  async function uploadData() {
+  const [showImageCapture, setShowImageCapture] = useState(false);
+
+  const captureImage = async () => {
+    try {
+      const photo = await takePhoto();
+      setImageurl(photo);
+      setShowImageCapture(false);
+    } catch (error) {
+      console.error("Error capturing image:", error);
+    }
+  };
+
+  const takePhoto = () =>
+    new Promise<string>((resolve, reject) => {
+      const input = document.createElement("input");
+      input.type = "file";
+      input.accept = "image/*";
+
+      input.onchange = (event) => {
+        const fileInput = event.target as HTMLInputElement;
+        const files = fileInput.files;
+
+        if (files && files.length > 0) {
+          const file = files[0];
+          const reader = new FileReader();
+
+          reader.onload = () => {
+            const photoUrl = reader.result as string;
+            resolve(photoUrl);
+          };
+
+          reader.onerror = () => {
+            reject(new Error("Error reading the file."));
+          };
+
+          reader.readAsDataURL(file);
+        } else {
+          reject(new Error("No file selected."));
+        }
+      };
+
+      input.click();
+    });
+
+  const uploadData = async () => {
     if (!imageUpload) return;
 
     const imageRef = ref(storage, `doemateriais/images/${imageUpload.name}`);
 
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        const material = addDoc(doadosCollection, {
-          nome_material,
-          quantidade,
-          medida,
-          descricao,
-          data_limite,
-          telefone,
-          cidade,
-          estado,
-          rua,
-          numero,
-          url,
-        });
+    try {
+      const snapshot = await uploadBytes(imageRef, imageUpload);
+      const url = await getDownloadURL(snapshot.ref);
+      const material = await addDoc(doadosCollection, {
+        nome_material,
+        quantidade,
+        medida,
+        descricao,
+        data_limite,
+        telefone,
+        cidade,
+        estado,
+        rua,
+        numero,
+        url,
       });
-    });
-  }
+      // Limpar o formulário após o envio bem-sucedido
+      setNome_material("");
+      setQuantidade("");
+      setMedida("");
+      setDescricao("");
+      setData_limite("");
+      setTelefone("");
+      setCidade("");
+      setEstado("");
+      setRua("");
+      setNumero("");
+      setImageUpload(undefined);
+      setImageurl(null);
+    } catch (error) {
+      console.error("Error uploading data:", error);
+    }
+  };
 
   return (
     <div className="doar-form">
@@ -52,6 +111,7 @@ export default function DoarForm() {
           <Form.Control
             type="text"
             placeholder="Nome..."
+            value={nome_material}
             onChange={(e) => setNome_material(e.target.value)}
           />
         </Form.Group>
@@ -63,6 +123,7 @@ export default function DoarForm() {
                 type="number"
                 placeholder="0,00"
                 min={0}
+                value={quantidade}
                 onChange={(e) => setQuantidade(e.target.value)}
               />
             </Form.Group>
@@ -72,6 +133,7 @@ export default function DoarForm() {
               <Form.Label>Medida</Form.Label>
               <Form.Select
                 aria-label="Default select example"
+                value={medida}
                 onChange={(e) => setMedida(e.target.value)}
               >
                 <option>Selecione</option>
@@ -88,6 +150,7 @@ export default function DoarForm() {
             as="textarea"
             placeholder="Descreva o material que você irá doar, inclua dimensões, cores, estado de conservação, entre outros"
             rows={3}
+            value={descricao}
             onChange={(e) => setDescricao(e.target.value)}
           />
         </Form.Group>
@@ -97,6 +160,7 @@ export default function DoarForm() {
           <Form.Control
             type="date"
             placeholder="Selecione uma data"
+            value={data_limite}
             onChange={(e) => setData_limite(e.target.value)}
           />
         </Form.Group>
@@ -106,6 +170,7 @@ export default function DoarForm() {
           <Form.Control
             type="text"
             placeholder="(00)0000-000"
+            value={telefone}
             onChange={(e) => setTelefone(e.target.value)}
           />
         </Form.Group>
@@ -117,6 +182,7 @@ export default function DoarForm() {
               <Form.Control
                 type="text"
                 placeholder="Nome da cidade"
+                value={cidade}
                 onChange={(e) => setCidade(e.target.value)}
               />
             </Form.Group>
@@ -127,6 +193,7 @@ export default function DoarForm() {
               <Form.Control
                 type="text"
                 placeholder="AA"
+                value={estado}
                 onChange={(e) => setEstado(e.target.value)}
               />
             </Form.Group>
@@ -139,6 +206,7 @@ export default function DoarForm() {
               <Form.Control
                 type="text"
                 placeholder="Nome da rua"
+                value={rua}
                 onChange={(e) => setRua(e.target.value)}
               />
             </Form.Group>
@@ -150,6 +218,7 @@ export default function DoarForm() {
                 type="number"
                 placeholder="00"
                 min={0}
+                value={numero}
                 onChange={(e) => setNumero(e.target.value)}
               />
             </Form.Group>
@@ -158,26 +227,49 @@ export default function DoarForm() {
 
         <Form.Group controlId="formFile" className="mb-3">
           <Form.Label>Foto do material</Form.Label>
-          <Form.Control
-            capture
-            type="file"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              const fileInput = event.target as HTMLInputElement;
-              const files = fileInput.files;
-
-              if (files && files.length > 0) {
-                const file = files[0];
-                setImageUpload(file);
-              }
-            }}
-          />
+          <div>
+            {showImageCapture && (
+              <div>
+                <Button variant="primary" onClick={captureImage}>
+                  Capturar Foto
+                </Button>{" "}
+                <Button
+                  variant="secondary"
+                  onClick={() => setShowImageCapture(false)}
+                >
+                  Cancelar
+                </Button>
+              </div>
+            )}
+            {!showImageCapture && (
+              <div>
+                {imageurl && (
+                  <img
+                    src={imageurl}
+                    alt="Captured"
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                )}
+                <Button
+                  variant="primary"
+                  onClick={() => setShowImageCapture(true)}
+                >
+                  Escolher Foto
+                </Button>
+                {imageurl && (
+                  <Button variant="secondary" onClick={() => setImageurl(null)}>
+                    Remover Foto
+                  </Button>
+                )}
+              </div>
+            )}
+          </div>
         </Form.Group>
 
         <div className="d-flex justify-content-center gap-2 w-100 mt-4">
-          <Button variant="primary" onClick={(e) => uploadData()}>
+          <Button variant="primary" onClick={uploadData}>
             Enviar
           </Button>
-
           <Button variant="danger" type="reset">
             Limpar
           </Button>
